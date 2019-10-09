a28b8982158efae3173fad043e94d1c9f66c4648
silentsaber <805898882@qq.com>
2019-10-09 15:20:44 +0800

    修改了顶层模块main.m，增加了参数声明，解释了各参数的定义。在顶层模块连接了已有的几个模块:数据源、卷积编码器、电平映射、信道传输。还需要增加的内容包括维特比译码（硬判决、软判决），以及CRC校验码的添加和各种测试

--- main.m
+++ main.m
@@ -1,5 +1,16 @@
+%% 该文件用于级联所有模块
+
+%% 参数设定
+datalen=20;%随机生成01序列的长度
+eff=2;%卷积编码效率，取值{2,3},2代表1/2编码，3代表1/3编码
+tail=1;%卷积编码发端是否收尾，取值{0,1}，0代表不收尾，1代表收尾
+bitmode=1;%电平映射模式，取值{1,2,3}，1代表1bit/符号，2代表2bit/符号，3代表3bit/符号
+channelmode=1;%信道传输模式，取值{1,2}，1表示场景1:即依次通信过程中φ不变，每次通信有独立的φ 2表示场景2:即再一次通信过程中，每次信道使用φ均独立变化
+theta=pi/60; %即θ
+sigma=0.001; %即σ
+
 %% 连接所有的模块，构成整个通信系统
-data=sourcedata(20);
-convres=model_conv(data,2,1);
-mapres=model_map(convres,2);
-channelres=channel(mapres,1,pi/60,0.001);
+data=sourcedata(datalen);  %随机生成数据流
+convres=model_conv(data,eff,tail);  %卷积编码
+mapres=model_map(convres,bitmode);  %电平映射
+channelres=channel(mapres,channelmode,theta,sigma);  %信道传输
